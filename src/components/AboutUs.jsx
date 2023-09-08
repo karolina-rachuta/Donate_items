c26ec907766910078e871c5ebbd460e223c9e3ed
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-09-08 23:30:47 +0400

    layout and styles for abour us section

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,8 +1,18 @@
+import decoration from "../assets/Decoration.svg";
+import signature from "../assets/Signature.svg"
+
 const AboutUs = () => {
     return (
-        <div className="container" id="aboutUs" style={{height:"500px", backgroundColor:"lightpink"}}>
-            <h1>o nas</h1>
-
+        <div className="container" id="aboutUs">
+            <div className="container__aboutUs">
+                <div className="box__text">
+                    <span>About us</span>
+                    <img src={decoration} alt="Decoration"/>
+                    <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts
+                        black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                    <img className="img__signature" src={signature} alt="Signature"/>
+                </div>
+            </div>
         </div>
     )
 }
