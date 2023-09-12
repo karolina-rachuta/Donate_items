7e4b687f9a837ac67d25a99d3152f6b4593a544d
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-09-12 21:19:58 +0400

    started contact section layout and styles

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -1,8 +1,29 @@
+import decoration from "../../assets/Decoration.svg"
+
 const Contact = () => {
     return (
-        <div className="container" id="contact" style={{height:"500px", backgroundColor:"lightgoldenrodyellow"}}>
-            <h1>kontakt</h1>
-
+        <div className="container container__contacts" id="contact">
+            <div className="contact__form">
+                <div>
+                <h3>Contact with us</h3>
+                <img src={decoration} alt="Decoration"/>
+                <div>
+                    <label htmlFor="name">First Name</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div>
+                    <label htmlFor="email">Enter your email</label>
+                    <input type="email" id="email"/>
+                </div>
+                <div>
+                    <label htmlFor="message">Enter your message</label>
+                    <textarea name="messega" id="" cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                    <button className="btn">Send</button>
+                </div>
+                </div>
+            </div>
         </div>
     )
 }
