4e7d11c69f38355444a249968323cd9b9c35c348
Karolina Rachuta <karolina.rachuta@gmail.com>
2023-10-11 17:20:34 +0400

    added hamburger menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link as RouterLink, useLocation, useNavigate} from "react-router-dom";
 import {Link as ScrollLink} from "react-scroll";
 import {useUser} from "../AuthProvider.jsx";
@@ -10,7 +10,7 @@ const Navigation = () => {
     const user = useUser()
     console.log(user);
     const location = useLocation();
-
+    const [menuOpen, setMenuOpen] = useState(false);
     const handleLogOut = () => {
         signOut(auth).then(() => {
             navigate("/logout");
@@ -22,6 +22,10 @@ const Navigation = () => {
     const isLoginPage = location.pathname === '/login';
     const isRegisterPage = location.pathname === '/register';
 
+    const handleHamburger = () => {
+        setMenuOpen(prevState => !prevState)
+    }
+
     return (
         <nav>
             <ul className="menu menu__login">
@@ -30,18 +34,33 @@ const Navigation = () => {
                         <li className="menu__email">Hello {user.email}!</li>
                         <li className="menu__oddaj menu__ramka"><RouterLink to="/donate">Give Items</RouterLink></li>
                         <li className="menu__wyloguj" onClick={handleLogOut}>Log out</li>
+                        <li>
+                            <div className="hamburger" onClick={handleHamburger}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </li>
                     </>
                 ) : (
                     <>
-                        <li className={isLoginPage ? "menu__ramka" : "menu__ramka1"}><RouterLink to="/login">Login</RouterLink>
+                        <li className={isLoginPage ? "menu__ramka" : "menu__ramka1"}><RouterLink
+                            to="/login">Login</RouterLink>
                         </li>
                         <li className={isRegisterPage ? "menu__ramka" : "menu__ramka1"}><RouterLink
                             to="/register">Register</RouterLink></li>
+                        <li>
+                            <div className="hamburger" onClick={handleHamburger}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </li>
                     </>
                 )}
             </ul>
 
-            <ul className="menu menu__home">
+            <ul className={menuOpen ? "menu menu__home open" : "menu menu__home"}>
                 <li className={isHomePage ? "menu__ramka2" : "menu__ramka3"}><RouterLink to="/">Home</RouterLink></li>
                 <li><ScrollLink to="donate">How to Start</ScrollLink></li>
                 <li><ScrollLink to="aboutUs">About Us</ScrollLink></li>
